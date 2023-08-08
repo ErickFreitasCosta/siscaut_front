ad3dc31e8d62571e815a63df0a0ff873967b0675
ErickFreitasCosta <erickifpa2021@gmail.com>
2023-08-08 13:26:15 -0300

    adicionei a logo

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -31,43 +31,6 @@ const Header = () => {
             <Row>
               <Col lg="3" xl="1">
 
-                {/* <Card className="card-stats mb-4 mb-xl-0">
-
-
-                  <CardBody>
-                    <Row>
-                      <div className="col">
-                        <CardTitle
-                          tag="h5"
-                          className="text-uppercase text-muted mb-0"
-                        >
-                          Cadastrar
-                        </CardTitle>
-                        <span className="h5 font-weight-bold mb-0">
-                          Responsável
-                        </span>
-                      </div>
-
-
-                      <Col className="col-auto">
-                        <div className="icon icon-shape bg-success text-white rounded-circle shadow">
-                          <i className="ni ni-single-02" />
-                        </div>
-                      </Col>
-
-                    </Row>
-                    <p className="mt-3 mb-0 text-muted text-sm">
-                      <span className="text-success mr-2">
-                        <i className="fa fa-arrow-up" /> 3.48%
-                      </span>{" "}
-                      <span className="text-nowrap">Since last month</span>
-                    </p>
-                  </CardBody>
-
-
-                </Card> */}
-
-
               </Col>
               <Col lg="6" xl="2">
                 <Card className="card-stats mb-4 mb-xl-0">
